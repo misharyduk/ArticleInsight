abcd3cb4108372a09118a7e88038a58339282ed0
misharyduk <misharyduk64@gmail.com>
2024-10-05 16:02:19 +0300

    added builder to response dto

--- articles/src/main/java/com/mike/articleinsight/articles/dto/ArticleResponseDto.java
+++ articles/src/main/java/com/mike/articleinsight/articles/dto/ArticleResponseDto.java
@@ -1,11 +1,13 @@
 package com.mike.articleinsight.articles.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Builder
 public class ArticleResponseDto {
 
     private Long id;

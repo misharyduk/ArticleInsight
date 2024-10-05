c2b4dc3f9a4b8093b1bd6bd2bef35c0dc18ccd22
misharyduk <misharyduk64@gmail.com>
2024-10-05 15:43:07 +0300

    init service with CRUB interface

---
+++ articles/src/main/java/com/mike/articleinsight/articles/service/ArticleService.java
@@ -0,0 +1,18 @@
+package com.mike.articleinsight.articles.service;
+
+import com.mike.articleinsight.articles.dto.ArticleRequestDto;
+import com.mike.articleinsight.articles.dto.ArticleResponseDto;
+
+import java.util.List;
+
+public interface ArticleService {
+
+    List<ArticleResponseDto> getArticles();
+
+    ArticleResponseDto getArticleById(Long articleId);
+
+    Long createArticle(ArticleRequestDto articleRequestDto);
+
+    void deleteArticleById(Long id);
+
+}

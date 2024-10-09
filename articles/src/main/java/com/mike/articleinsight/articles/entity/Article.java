47b20ad80955700bf4846148f4d2b9e53658343f
misharyduk <misharyduk64@gmail.com>
2024-10-09 21:42:28 +0300

    remove insert/update restrictions on all fields

--- articles/src/main/java/com/mike/articleinsight/articles/entity/Article.java
+++ articles/src/main/java/com/mike/articleinsight/articles/entity/Article.java
@@ -14,14 +14,14 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "article_sequence_generator")
     @Id
     private Long id;
-    @Column(name = "article_title", insertable = false)
+    @Column(name = "article_title")
     private String title;
-    @Column(name = "article_text", columnDefinition = "TEXT", updatable = false)
+    @Column(name = "article_text", columnDefinition = "TEXT")
     private String text;
     @Column(name = "article_category")
     private String category;
-    @Column(name = "article_author", updatable = false)
+    @Column(name = "article_author")
     private String author;
-    @Column(name = "article_publishing_date", updatable = false)
+    @Column(name = "article_publishing_date")
     private Date publishingDate;
 }

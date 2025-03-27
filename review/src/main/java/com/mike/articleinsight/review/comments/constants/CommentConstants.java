ba724163349a6eff0b9c5d0b0fd1468e0725e6d4
misharyduk <misharyduk64@gmail.com>
2025-03-27 15:08:44 +0200

    added class for comment constant values

---
+++ review/src/main/java/com/mike/articleinsight/review/comments/constants/CommentConstants.java
@@ -0,0 +1,7 @@
+package com.mike.articleinsight.review.comments.constants;
+
+public class CommentConstants {
+
+    public static final String COMMENT_201_MESSAGE = "Comment has been successfully posted";
+
+}
